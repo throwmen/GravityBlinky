19d92c0370d47ec2399929d18c63314c1ea790f7
throwmen <j.oliveros@ieciudaddonbosco.edu.co>
2023-11-06 23:22:35 +0000

    Ingreso clase LCD

--- main.cpp
+++ main.cpp
@@ -5,21 +5,23 @@
 
 #include "mbed.h"
 #include <iostream>
-
+#include "TextLCD.h"
+using namespace std::chrono;
 // Blinking rate in milliseconds
 #define BLINKING_RATE     100ms
 
 
 UnbufferedSerial serial(USBTX, USBRX, 9600);
+TextLCD lcd (D2, D3, D4, D5, D6, D7, TextLCD::LCD16x2);
 AnalogIn lightSensor1(A0);
 AnalogIn lightSensor2(A1);
 Timer timer;
 
 int main()
 {
+    std::cout << "Inicia el cálculo de la gravedad: " << std::endl;
     bool paso1 = true;
     bool paso2 = false;
-    std::cout << "Inicia el cálculo de la gravedad: " << std::endl;
     float treshold = 0.2;
     const float height = 0.1;
 
@@ -32,10 +34,15 @@ int main()
         }
         else if (lightSensor2.read() < treshold && paso2){
             timer.stop(); 
-            float timeInSeconds = duration_cast<milliseconds>(timer.elapsed_time()).count()/1000;
+            float timeInSeconds = duration_cast<milliseconds>(timer.elapsed_time()).count();
+            timeInSeconds /= 1000;
             float gravity = 2*height/pow(timeInSeconds,2);
             std::cout << "La gravedad es de: " << gravity << " m/s^2" << std::endl;
-            timer.reset();
+            lcd.cls();
+            lcd.locate(0, 0);
+            lcd.printf("La aceleración es: ");
+            lcd.locate(0, 1);
+            lcd.printf("%f .m/s", gravity);
             paso1 = true; 
             paso2 = false;       
         }
